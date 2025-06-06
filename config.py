04eb8736ab7a7ca558a91f73d9bd0d1d2e64cac7
ɪᴍ_ᴘʀᴏᴛʀɪᴄᴋs <115210962+Protricks@users.noreply.github.com>
2025-06-06 12:41:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,27 +5,27 @@ load_dotenv()
 
 
 ### ❖ ➥
-API_ID = int(getenv("API_ID", None))
+API_ID = int(getenv("API_ID", "21432914"))
 
 ### ❖ ➥
-API_HASH = getenv("API_HASH", None)
+API_HASH = getenv("API_HASH", "4d65415fcce04e8076acee5b6688c1b1")
 
 ### ❖ ➥
-STRING_SESSION = getenv("STRING_SESSION", None)
+STRING_SESSION = getenv("STRING_SESSION", "BQFHClIAOVIwPyCi1BK1xWeEGKBWP-5NYejJjj68E8zA27dmBaBE1vosVwJC2OawM6oC9lCnYS_xUolMO-Wnt7-TuxbgTMG25OWwoEImg7zseeIz_-IqfcunjfyuqnLkYDHaF96kuFnk0UsBxmDFe2dE6FHz2cWoZd8CRKmg7EyV2HXS5zXaD92DMHxt7Ai_m3ttlAh62aFeDSwYyR8sqmckZ-zr4MVCi7q5aDVmb_DGOoJracxW9DeR3kWa_c3-CdnSlZCSRcs0quu-IMpGJg08t6zYW8aSsaXuh-9bc65ULbhCIBnq8gi43uyFUNb9mxgJB4KOZbm7Eysx4DuAdU0HsUsD1gAAAAFO0ERhAA")
 
 ### ❖ ➥
-OWNER_ID = int(getenv("OWNER_ID", "6922271843"))
+OWNER_ID = int(getenv("OWNER_ID", "6035523795"))
 
 ### ❖ ➥
-MONGO_URL = getenv("MONGO_URL", None)
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://EXONTESTMONGO:EXONTESTMONGO@cluster0.bviw7ic.mongodb.net/?retryWrites=true&w=majority")
 
 ### ❖ ➥
-SUPPORT_GRP = getenv("SUPPORT_GRP", "THE_FRIENDZ")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "Nxt_Bots")
 
 ### ❖ ➥
-UPDATE_CHNL = getenv("UPDATE_CHNL", "ROY_EDITX")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "Nxt_Bots")
 
 ### ❖ ➥
-OWNER_USERNAME = getenv("OWNER_USERNAME", "LOCO_X_PILOT")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "Mafia_xDD")
 
 
